a461b0ce54540fad5b5b717fd8e9b696664b96a8
Luis Pitta Gros <luispittagroz@gmail.com>
2023-07-11 12:50:03 +0100

    DRY get controller

--- apps/backend/src/controllers/jobs/get.ts
+++ apps/backend/src/controllers/jobs/get.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express'
 import { JobType } from 'reedsy-types'
 import { getGroupedJobs } from '@/services/jobs'
 
-export const getExports = (request: Request, response: Response) => {
-  const groupedJobs = getGroupedJobs(JobType.EXPORT)
+const getJobs = (request: Request, response: Response, type: JobType) => {
+  const groupedJobs = getGroupedJobs(type)
 
   if (!groupedJobs) {
     return response.status(404).json({ error: 'No jobs found' })
@@ -12,12 +12,8 @@ export const getExports = (request: Request, response: Response) => {
   response.status(200).json(groupedJobs)
 }
 
-export const getImports = (request: Request, response: Response) => {
-  const groupedJobs = getGroupedJobs(JobType.IMPORT)
+export const getExports = (request: Request, response: Response) =>
+  getJobs(request, response, JobType.EXPORT)
 
-  if (!groupedJobs) {
-    return response.status(404).json({ error: 'No jobs found' })
-  }
-
-  response.status(200).json(groupedJobs)
-}
+export const getImports = (request: Request, response: Response) =>
+  getJobs(request, response, JobType.IMPORT)
